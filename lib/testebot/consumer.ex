121b686cf67c6c556d3b48fdf980706e61c98ce8
Marcel Juca <marceljuca@gmail.com>
2022-04-13 05:23:51 -0700

    Primeiro teste utilizando EldenRingAPI

--- lib/testebot/consumer.ex
+++ lib/testebot/consumer.ex
@@ -8,14 +8,44 @@ defmodule Testebot.Consumer do
     end
 
     def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
-        case msg.content do
-            "!oi" -> Api.create_message(msg.channel_id, "Ola, meu consagrado")
-            _ -> :ignore
+        cond do
+            msg.content == "!oi" -> Api.create_message(msg.channel_id, "Ola, meu consagrado")
+
+            String.starts_with?(msg.content, "!boss ") -> eldenring_boss_query(msg)
+            String.starts_with?(msg.content, "!boss") ->
+            Api.create_message(msg.channel_id, "Use **!boss <Nome>**.")
+
+            true -> :ignore
         end
     end
 
     def handle_event(_event) do
         :noop
+    end
+
+    def eldenring_boss_query(msg) do
+        corpo = String.split(msg.content, " ", parts: 2)
+        bossName = Enum.fetch!(corpo, 1)
+        resp = HTTPoison.get!("https://eldenring.fanapis.com/api/bosses?name=#{bossName}")
+
+        {:ok, map} = Poison.decode(resp.body)
+
+        case map["success"] do
+            true ->
+                #obtendo chave de dicionario, dentro de lista obtida atraves de outra chave de dicionario.
+                #Formato da API {"data":[{"name": "exemplo"}]}
+                name = Enum.at(map["data"],0)["name"]
+                description = Enum.at(map["data"],0)["description"]
+                location = Enum.at(map["data"],0)["location"]
+                image = Enum.at(map["data"],0)["image"]
+                Api.create_message(msg.channel_id, "Name: #{name}
+            Description: #{description}
+            Location: #{location}
+            #{image}")
+
+            false ->
+                Api.create_message(msg.channel_id, "Comando Invalido!")
+        end
     end
 
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Testebot.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :httpoison],
       mod: {Testebot.Application, []}
     ]
   end
@@ -22,9 +22,9 @@ defmodule Testebot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nostrum, "~> 0.5.1"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:nostrum, "~> 0.5.1"},
+      {:httpoison, "~> 1.8"},
+      {:poison, "~> 5.0"}
     ]
   end
 end
