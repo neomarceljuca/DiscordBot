a5af41f9fa1942defb73b7ab6ff15a6e2cc0224e
Marcel Juca <marceljuca@gmail.com>
2022-05-04 17:44:13 -0700

    atualizar .gitignore para acomodar exemplo de config.exs

---
+++ config/configexample.exs
@@ -0,0 +1,19 @@
+import Config
+
+config :nostrum,
+  #EldenRingBot
+  applicationID: "",
+  publicKey: "",
+  token: "",
+
+  #OpenWeatherAPI
+  openWeatherToken: "",
+
+  #RapidAPI
+  XRapidAPIKey: "",
+
+  #Preview Link API
+  linkPreviewKey: "",
+
+  #wolram
+  wolframID: ""
